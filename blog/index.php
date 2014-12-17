9fcd4d111afd077fb97b1ea2bd4bb8113caa9a2b
binleen <158497182@qq.com>
2014-12-17 14:26:59 +0800

    blog

--- blog/index.php
+++ blog/index.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/protected/components/globals.php';
 // change the following paths if necessary
 //定义一个yii类的路径
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/../yiirestful/framework/yii.php';
 //定义了应用的配置文件路径
 $config=dirname(__FILE__).'/protected/config/main.php';
 
